0983ccf27a82cf687aaeda121a4db729e121202c
Kethlyn Justo <kethdiniz06@gmail.com>
2026-07-07 14:14:24 -0300

    Initial commit: base structure for banking study project

---
+++ estudoBanco/src/main/java/com/transacao/estudoBanco/EstudoBancoApplication.java
@@ -0,0 +1,13 @@
+package com.transacao.estudoBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoBancoApplication.class, args);
+	}
+
+}
---
+++ estudoBanco/src/test/java/com/transacao/estudoBanco/EstudoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transacao.estudoBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
